85eb3a5944adb79da5ec7f58c43348b4304ee85f
Jonathan Sick <jonathansick@mac.com>
2012-08-31 14:55:55 -0400

    Hook up montage offset pipeline in calibrationfactory for fw100k_medsky

--- montageoffsets.py
+++ montageoffsets.py
@@ -20,32 +20,34 @@ def main():
     work_montage("Ks", levelOnly=False)
 
 
-def work_montage(band, levelOnly=True):
+def work_montage(rootName, blockSel, band, mosaicName, workDir,
+        levelOnly=True):
     origDirectory = os.getcwd()
     if levelOnly:
         skyType = "scalar"
     else:
         skyType = "planar"
-    workDir = "skyoffsets/montage/%s_%s" % (band,skyType)
+    # workDir = "skyoffsets/montage/%s_%s" % (band,skyType)
     projDir = "proj"
     diffDir = "diff"
     correctedDir = "corr"
-    rawTblPath = "m31_%s.raw.tbl"%band
-    projTblPath = "m31_%s.proj.tbl"%band
-    correctedTblPath = "M31_%s.corr.tbl"%band
-    hdrPath = "m31_%s.hdr"%band
-    projStatsPath = "m31_%s.projstats"%band
-    diffsPath = "m31_%s.diffs"%band
-    diffFitsPath = "m31_%s.difffits"%band
-    correctionsPath = "m31_%s.corrections"%band
-    mosaicPath = "m31_%s_montage_%s.fits"%(band,skyType)
+    rawTblPath = "m31_%s.raw.tbl" % band
+    projTblPath = "m31_%s.proj.tbl" % band
+    correctedTblPath = "M31_%s.corr.tbl" % band
+    hdrPath = "m31_%s.hdr" % band
+    projStatsPath = "m31_%s.projstats" % band
+    diffsPath = "m31_%s.diffs" % band
+    diffFitsPath = "m31_%s.difffits" % band
+    correctionsPath = "m31_%s.corrections" % band
+    mosaicPath = "m31_%s_montage_%s.fits" % (band, skyType)
     
-    if os.path.exists(workDir) is False:
-        print "Swarp pre-re-sampling"
-        # need to create resampled image set
-        resample_blocks(band, workDir)
+    # if not os.path.exists(workDir):
+    print "Swarp pre-re-sampling"
+    # need to create resampled image set
+    print "workDir", workDir
+    resample_blocks(blockSel, band, workDir)
 
-    os.chdir(workDir) # change
+    os.chdir(workDir)  # change
 
     # make metadata table
     if os.path.exists(rawTblPath) is False:
@@ -56,9 +58,10 @@ def work_montage(band, levelOnly=True):
     # Create output image template
     if os.path.exists(hdrPath) is False:
         print "mMakeHdr"
-        print montage.mMakeHdr(rawTblPath, hdrPath, north_aligned=True,system="EQUJ")
+        print montage.mMakeHdr(rawTblPath, hdrPath, north_aligned=True,
+                system="EQUJ")
     print "mHdrCheck"
-    print montage.mHdrCheck(hdrPath) # verify that the WCS is valid
+    print montage.mHdrCheck(hdrPath)  # verify that the WCS is valid
     # reproject images into TAN system
     if os.path.exists(projDir) is False:
         os.makedirs(projDir)
@@ -66,8 +69,10 @@ def work_montage(band, levelOnly=True):
         print montage.mProjExec(rawTblPath,
                     hdrPath, projDir,
                     projStatsPath,
-                    raw_dir=None, debug=False, exact=False, whole=False, border=None,
-                    restart_rec=None, status_file=None, scale_column=None, mpi=False,
+                    raw_dir=None, debug=False, exact=False, whole=False,
+                    border=None,
+                    restart_rec=None, status_file=None, scale_column=None,
+                    mpi=False,
                     n_proc=8)
     # Create a new image table with Montage's reprojected images
     if os.path.exists(projTblPath) is False:
@@ -75,8 +80,8 @@ def work_montage(band, levelOnly=True):
     # Detect overlaps and compute difference images
     if os.path.exists(diffsPath) is False:
         print "mOverlaps"
-        print montage.mOverlaps(projTblPath, diffsPath, exact=True, debug_level=None,
-              status_file=None)
+        print montage.mOverlaps(projTblPath, diffsPath, exact=True,
+                debug_level=None, status_file=None)
         if os.path.exists(diffDir) is False: os.makedirs(diffDir)
         print "mDiffExec"
         print montage.mDiffExec(diffsPath, hdrPath, diffDir, proj_dir=projDir,
@@ -84,7 +89,8 @@ def work_montage(band, levelOnly=True):
     # Fit the difference images
     if os.path.exists(diffFitsPath) is False:
         #print "mDiffFitExec"
-        #print montage.mDiffFitExec(diffsPath, diffFitsPath, diffDir, debug=True)
+        #print montage.mDiffFitExec(diffsPath, diffFitsPath, diffDir,
+        #debug=True)
         print "mFitExec"
         print montage.mFitExec(diffsPath, diffFitsPath, diffDir)
     # Model background corrections
@@ -106,79 +112,54 @@ def work_montage(band, levelOnly=True):
 
     # Add to the mosaicdb
     fullMosaicPath = os.path.join(workDir, mosaicPath)
-    mosaicName = "montage_%s_%s" % (skyType, band)
     mosaicDB = mosaicdb.MosaicDB(dbname="m31", cname="mosaics")
-    mosaicDoc = mosaicDB.collection.find({"_id": mosaicName})
-    if mosaicDoc is not None:
+    if mosaicDB.collection.find({"_id": mosaicName}).count() > 0:
         mosaicDB.collection.remove({"_id": mosaicName})
     doc = {"_id": mosaicName, "image_path": fullMosaicPath,
             "subsampled_path": fullMosaicPath,
-            "FILTER": band}
+            "FILTER": band,
+            "calibrated_set": rootName,
+            "montage_type": skyType}
     mosaicDB.collection.insert(doc)
 
 
-
-def copy_blocks(band, workDir):
-    """Resamples blocks into the TAN system, compatible with Montage."""
-    if os.path.exists(workDir) is False: os.makedirs(workDir)
-    blockDB = blockdb.BlockDB(dbname="skyoffsets", cname="wircam_blocks")
-    
-    blockDocs = blockDB.find_blocks({"FILTER": band,
-        "field": {"$exists":1, "$nin":["M31-44","M31-45","M31-46"],
-            "$in": ALLFIELDS}
-        })
-
-    for blockName, doc in blockDocs.iteritems():
-        #print blockName
-        #print doc.keys()
-        imagePath = doc['image_path']
-        weightPath = doc['weight_path']
-        newImagePath = os.path.join(workDir, os.path.basename(imagePath))
-        newWeightPath = os.path.join(workDir, os.path.basename(weightPath))
-        if os.path.exists(newImagePath) is False:
-            print imagePath, newImagePath
-            shutil.copy(imagePath, newImagePath)
-            #shutil.copy(weightPath, newWeightPath)
-            blockDB.collection.update({"_id":doc["_id"]},
-                    {"$set": {"montage_input_image_path": newImagePath,
-                        "montage_input_weight_path": newWeightPath}})
-
-def resample_blocks(band, workDir, pixScale=1.):
+def resample_blocks(blockSel, band, workDir, pixScale=1.):
     """Resamples blocks into the TAN system at a reduced pixel scale,
     compatible with Montage."""
-    if os.path.exists(workDir) is False: os.makedirs(workDir)
-    blockDB = blockdb.BlockDB(dbname="skyoffsets", cname="wircam_blocks")
-    
-    blockDocs = blockDB.find_blocks({"FILTER": band,
-        "field": {"$exists":1, "$nin":["M31-44","M31-45","M31-46"],
-            "$in": ALLFIELDS}
-        })
+    if not os.path.exists(workDir):
+        print "Make dir", workDir
+        os.makedirs(workDir)
+    # blockDB = blockdb.BlockDB(dbname="skyoffsets", cname="wircam_blocks")
+    blockDB = blockdb.BlockDB()
+    blockDocs = blockDB.find_blocks(blockSel)
     
     ids, imagePaths, weightPaths = [], [], []
     for blockName, doc in blockDocs.iteritems():
         ids.append(doc['_id'])
+        print doc['_id']
         imagePaths.append(doc['image_path'])
         weightPaths.append(doc['weight_path'])
     configs = {"RESAMPLE_DIR": workDir,
             "PROJECTION_TYPE": "TAN",
-            "COMBINE":"N", "RESAMPLE":"Y","WEIGHT_TYPE":"MAP_WEIGHT",
-            "NTHREADS":"8", "SUBTRACT_BACK":"N",
-            "WRITE_XML":"N", "VMEM_DIR":"/Volumes/Zaphod/tmp",
+            "COMBINE": "N", "RESAMPLE": "Y", "WEIGHT_TYPE": "MAP_WEIGHT",
+            "NTHREADS": "8", "SUBTRACT_BACK": "N",
+            "WRITE_XML": "N", "VMEM_DIR": "/Volumes/Zaphod/tmp",
             "MEM_MAX": "8000",
-            'PIXEL_SCALE':"%.2f" % pixScale,
+            'PIXEL_SCALE': "%.2f" % pixScale,
             'PIXELSCALE_TYPE': 'MANUAL'}
-    swarp = Swarp(imagePaths, "resamp_%s"%band+".fits",
+    swarp = Swarp(imagePaths, "resamp_%s" % band + ".fits",
             weightPaths=weightPaths, configs=configs,
             workDir=workDir)
     swarp.run()
 
     for dbid, imagePath in zip(ids, imagePaths):
         basename = os.path.splitext(os.path.basename(imagePath))[0]
-        resamplePath = os.path.join(workDir, basename+".resamp.fits")
-        resampledWeightPath = os.path.join(workDir, basename+".resamp.weight.fits")
-        os.remove(resampledWeightPath) # don't need weights
+        resamplePath = os.path.join(workDir, basename + ".resamp.fits")
+        resampledWeightPath = os.path.join(workDir,
+                basename + ".resamp.weight.fits")
+        os.remove(resampledWeightPath)  # don't need weights
         scale_image(resamplePath)
-        blockDB.collection.update({"_id":dbid},
+        blockDB.collection.update({"_id": dbid},
                 {"$set": {"montage_input_image_path": resamplePath}})
 
     # Clean up Swarp's input files
@@ -186,11 +167,11 @@ def resample_blocks(band, workDir, pixScale=1.):
     os.remove(os.path.join(workDir, "inputlist.txt"))
     os.remove(os.path.join(workDir, "weightlist.txt"))
 
+
 def scale_image(path):
     fits = pyfits.open(path)
     data = fits[0].data
     data[data == 0.] = np.nan
-    #fits[0].data = data * 10.**12
     fits[0].data = data
     fits.writeto(path, clobber=True)
 
