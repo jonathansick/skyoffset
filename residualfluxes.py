62ec7bd0cca80a91b2358e913b5fd245a7ffd967
Jonathan Sick <jonathansick@mac.com>
2012-11-09 17:33:40 -0500

    Updated mosaic residuals network map

--- residualfluxes.py
+++ residualfluxes.py
@@ -9,33 +9,22 @@ History
 
 """
 
-__all__ = ['']
 
-import numpy as np
 import math
 
 from mosaicdb import MosaicDB
-from difftools import Couplings # Scalar couplings
-from difftools import CoupledPlanes # Planar couplings
-from multisimplex import SimplexScalarOffsetSolver
+from difftools import Couplings  # Scalar couplings
 
-def main():
-    pass
 
 class ScalarResidualFlux(object):
     """Residual flux in a scalar-fit mosaic"""
     def __init__(self, mosaicName):
         super(ScalarResidualFlux, self).__init__()
+        print mosaicName
         self.mosaicName = mosaicName
         mosaicdb = MosaicDB()
         mosaicDoc = mosaicdb.collection.find_one({"_id": mosaicName})
-        solverCName = mosaicDoc['solver_cname']
-        solverDBName = mosaicDoc['solver_db']
-        
-        solver = SimplexScalarOffsetSolver(dbname=solverDBName,
-                cname=solverCName)
-        self.offsets = solver.find_best_offsets()
-
+        self.offsets = mosaicDoc['offsets']
         self.couplings = Couplings.load_doc(mosaicDoc['couplings'])
 
     def residual_fluxes(self):
@@ -59,15 +48,7 @@ class ScalarResidualFlux(object):
                 + self.offsets[lowerField])
         return residualLevels
 
-    def residual_levels_sb(self, pixScale=0.3):
-        """Returns the residual level difference in mag/arcsec^2."""
-        residualLevels = self.residual_levels()
-        residualSBs = {}
-        for pairKey, level in residualLevels.iteritems():
-            residualSBs[pairKey] = -2.5*math.log10(level/pixScale**2.)
-        return residualSBs
-
-    def relative_residuals(self):
+    def residual_rel_sigma(self):
         """The residual level relative to the coupling sigma."""
         relResiduals = {}
         for pairKey, diff in self.couplings.fieldDiffs.iteritems():
@@ -77,12 +58,18 @@ class ScalarResidualFlux(object):
                 + self.offsets[lowerField]) / sigma
         return relResiduals
 
+    def residual_rel_sblevel(self):
+        """Returns ratios of residual level difference / mean intensity level
+        in block.
+        """
+        relResiduals = {}
+        for pairKey, diff in self.couplings.fieldDiffs.iteritems():
+            upperField, lowerField = pairKey
+            fieldLevel = self.couplings.fieldLevels[pairKey]
+            relResiduals[pairKey] = math.fabs(diff - self.offsets[upperField]
+                + self.offsets[lowerField]) / math.fabs(fieldLevel)
+        return relResiduals
+
     def coupling_sigmas(self):
-        """docstring for coupling_sigmas"""
+        """The standard deviations of image overlaps."""
         return self.coupligns.fieldDiffSigmas
-
-
-if __name__ == '__main__':
-    main()
-
-
