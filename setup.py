0b9104c10791ad9933d1a75669fe43d9d5f1ff22
Jonathan Sick <jsick@lsst.org>
2016-04-28 18:08:29 +0000

    Remove astropy helper depenency for install

--- setup.py
+++ setup.py
@@ -5,21 +5,7 @@ import glob
 import os
 import sys
 
-import setuptools_bootstrap  # NOQA
 from setuptools import setup
-
-#A dirty hack to get around some early import/configurations ambiguities
-if sys.version_info[0] >= 3:
-    import builtins
-else:
-    import __builtin__ as builtins
-builtins._ASTROPY_SETUP_ = True
-
-import astropy  # NOQA
-from astropy_helpers.setup_helpers import (register_commands, adjust_compiler,
-                                           get_debug_option)
-from astropy_helpers.git_helpers import get_git_devstr
-from astropy_helpers.version_helpers import generate_version_py
 
 # Set affiliated package-specific settings
 PACKAGENAME = 'skyoffset'
@@ -36,37 +22,9 @@ VERSION = '0.1.dev'
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
 
-if not RELEASE:
-    VERSION += get_git_devstr(False)
-
-# Populate the dict of setup command overrides; this should be done before
-# invoking any other functionality from distutils since it can potentially
-# modify distutils' behavior.
-cmdclassd = register_commands(PACKAGENAME, VERSION, RELEASE)
-
-# Adjust the compiler in case the default on this platform is to use a
-# broken one.
-adjust_compiler(PACKAGENAME)
-
-# Freeze build information in version.py
-generate_version_py(PACKAGENAME, VERSION, RELEASE,
-                    get_debug_option(PACKAGENAME))
-
 # Treat everything in scripts except README.rst as a script to be installed
 scripts = [fname for fname in glob.glob(os.path.join('scripts', '*'))
            if os.path.basename(fname) != 'README.rst']
-
-
-from astropy_helpers.setup_helpers import get_package_info
-
-# Get configuration information from all of the various subpackages.
-# See the docstring for setup_helpers.update_package_files for more
-# details.
-package_info = get_package_info(PACKAGENAME)
-
-# Add the project-global data
-package_info['package_data'][PACKAGENAME] = ['data/*']
-
 
 setup(name=PACKAGENAME,
       version=VERSION,
@@ -81,7 +39,4 @@ setup(name=PACKAGENAME,
       license=LICENSE,
       url=URL,
       long_description=LONG_DESCRIPTION,
-      cmdclass=cmdclassd,
-      zip_safe=False,
-      use_2to3=True,
-      **package_info)
+      zip_safe=False)
